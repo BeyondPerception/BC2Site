22bcf4482f8414b1c7c5e611c0cd9aa4fb801392
Ronak Malik <ronakmalik4242@gmail.com>
2020-02-20 14:14:40 -0600

    Add login checker to Loginable

--- src/main/java/ml/dent/user/Loginable.java
+++ src/main/java/ml/dent/user/Loginable.java
@@ -1,5 +1,31 @@
 package ml.dent.user;
 
 public abstract class Loginable {
+	private String username;
+	private String password;
 
+	public Loginable(String username, String password) {
+		this.username = username;
+		this.password = password;
+	}
+
+	public boolean login(String username, String password) {
+		return this.username == username && this.password == password;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
 }
--- src/main/java/ml/dent/user/Team.java
+++ src/main/java/ml/dent/user/Team.java
@@ -1,12 +1,13 @@
 package ml.dent.user;
 
-public class Team {
+public class Team extends Loginable {
 
 	private int points;
 
 	private String teamName;
 
-	public Team() {
-		
+	public Team(String username, String password) {
+		super(username, password);
 	}
+
 }
